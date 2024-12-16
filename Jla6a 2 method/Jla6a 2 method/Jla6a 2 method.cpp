d58bce45652609aa82d91ef6e3f441dcea9e365a
()_() <96124047+kono6oyashi@users.noreply.github.com>
2024-12-17 02:48:21 +0500

    Add files via upload

---
+++ Jla6a 2 method/Jla6a 2 method/Jla6a 2 method.cpp
@@ -0,0 +1,35 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+
+
+double f(double x)
+{
+    return 0.25 * pow(x, 3) + x - 1.2502;
+}
+
+int main()
+{
+    double a = 0, b = 2, e = 0.00001, s;
+    if (f(a) * f(b) < 0)
+    {
+        while (abs(b - a) > e)
+        {
+            s = (a + b) / 2;
+            if (f(a) * f(s) < 0)
+            {
+                b = s;
+            }
+            else
+            {
+                a = s;
+            }
+        }
+        cout << s;
+    }
+    else
+    {
+        return 0;
+    }
+    return 0;
+}
