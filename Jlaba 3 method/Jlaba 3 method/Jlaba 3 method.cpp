be28f86ba25cec7bee1bc2c6aeb8599fd3428651
()_() <96124047+kono6oyashi@users.noreply.github.com>
2024-12-17 02:50:23 +0500

    Add files via upload

---
+++ Jlaba 3 method/Jlaba 3 method/Jlaba 3 method.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+double fi(double x)
+{
+    return 1.2502 - 0.25 * pow(x, 3);
+}
+
+int main()
+{
+    double x0 = 1.0, e = 0.00001, x = fi(x0);
+
+    while (abs(x - x0) > e)
+    {
+        x0 = x;
+        x = fi(x0);
+    }
+    cout << x;
+    return 0;
+}
